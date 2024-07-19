a89cecfdf4f87d04c61fd0501c365eaef9964d16
Maxwell <maxwellpatri@gmail.com>
2024-07-18 22:20:12 -0300

    Babel + WebPack

---
+++ transpiled/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+var _reactDom = _interopRequireDefault(require("react-dom"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+_reactDom["default"].render( /*#__PURE__*/_react["default"].createElement("h1", null, "Ol\xE1 Mundo!!!!!!!"), document.getElementById('root'));
