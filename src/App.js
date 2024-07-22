4b59fbfee57f8aa96a9fbd913128171f3c5aaf9c
Maxwell <maxwellpatri@gmail.com>
2024-07-21 21:39:48 -0300

    Utilização de States e Listagem de Componentes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Post from "./Post";
 import Title from "./Title";
 
 function App() {
+    const [posts, setPosts] = useState([
+        { id: Math.random(), title: "Titulo 01", subtitle: "Sub 01", likes: 10 },
+        { id: Math.random(), title: "Titulo 02", subtitle: "Sub 02", likes: 20 },
+        { id: Math.random(), title: "Titulo 03", subtitle: "Sub 03", likes: 30 }
+    ]);
+
+    console.log({ posts });
+
+    function handleRefresh() {
+        // setPosts([...posts, {
+        //     id: Math.random(),
+        //     title: `Titulo 0${posts.length + 1}`,
+        //     subtitle: `Sub 0${posts.length + 1}`,
+        //     likes: 10 + posts.length
+        // }])
+
+        setTimeout(() => {
+            setPosts((prevState) => [
+                ...prevState,
+                {
+                    id: Math.random(),
+                    title: `Titulo 0${posts.length + 1}`,
+                    subtitle: `Sub 0${posts.length + 1}`,
+                    likes: 10 + posts.length
+                }])
+        }, 2000)
+    }
+
     return (
         <>
             <Title site={{
@@ -11,18 +39,17 @@ function App() {
                 subtitle: "Posts da semana:"
             }} />
 
+            <button onClick={handleRefresh}>Atualizar</button>
             <hr />
 
-            <Post
-                title="Noticia 1"
-                subtitle="texto da noticia 2"
-                likes={10}
-            />
-            <Post
-                title="Noticia 2"
-                subtitle="texto da noticia 2"
-                likes={80}
-            />
+            {posts.map(post => (
+                <Post
+                    key={post.id}
+                    title={post.title}
+                    subtitle={post.subtitle}
+                    likes={post.likes}
+                />
+            ))}
         </>
     );
 }
